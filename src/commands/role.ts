b26d6aa786aca8c05a5d34ab8ea833941e18e922
Rex <rex@rexslab.com>
2022-11-24 11:17:36 +0200

    Role commands are now a group

--- src/commands/role.ts
+++ src/commands/role.ts
@@ -7,26 +7,18 @@ import {
     DiscordAPIError
 } from 'discord.js'
 import { ApplicationCommandOptionType } from 'discord.js'
-import { Discord, Guard, Slash, SlashChoice, SlashOption } from 'discordx'
+import { Discord, Guard, Slash, SlashGroup, SlashOption } from 'discordx'
 import { PrismaClient } from '@prisma/client'
-import console from 'console'
 
 const prisma = new PrismaClient()
 
 @Discord()
-export class ManageRole {
-    @Slash({ description: 'give or take a role' })
+@SlashGroup({ name: 'role', description: 'give or take a role' })
+@SlashGroup('role')
+export class role {
+    @Slash({ description: 'give a role' })
     @Guard(NotBot)
-    async role(
-        @SlashChoice('give', 'take')
-        @SlashOption({
-            name: 'action',
-            description: 'action',
-            required: true,
-            type: ApplicationCommandOptionType.String
-        })
-        action: string,
-
+    async give(
         @SlashOption({
             name: 'member',
             description: 'member',
@@ -45,47 +37,68 @@ export class ManageRole {
 
         interaction: CommandInteraction
     ) {
-        const roleToManage = role
-
-        const manageableRoles = await prisma.manageableRoles.findMany({
-            where: { roleId: roleToManage.id },
-            select: { managerRoleId: true }
+        await roleManage(interaction, 'give', role, user)
+    }
+    @Slash({ description: 'take a role' })
+    @Guard(NotBot)
+    async take(
+        @SlashOption({
+            name: 'member',
+            description: 'member',
+            required: true,
+            type: ApplicationCommandOptionType.User
         })
+        user: GuildMember,
 
-        const canManageRole = manageableRoles.some((manageableRole) => {
-            console.log('a')
-            if (
-                !(
-                    interaction.member &&
-                    interaction.member instanceof GuildMember &&
-                    interaction.member.roles instanceof GuildMemberRoleManager
-                )
-            )
-                return false
-            return (
-                interaction.member.permissions.has('ManageRoles', true) ||
-                interaction.member.roles.cache.some(
-                    (role) => role.id === manageableRole.managerRoleId
-                )
-            )
+        @SlashOption({
+            name: 'role',
+            description: 'role',
+            required: true,
+            type: ApplicationCommandOptionType.Role
         })
+        role: Role,
 
-        if (!canManageRole) return interaction.reply(`Sorry, you don't have permession to do that`)
+        interaction: CommandInteraction
+    ) {
+        await roleManage(interaction, 'take', role, user)
+    }
+}
 
-        try {
-            action === 'give'
-                ? await user.roles.add(roleToManage)
-                : await user.roles.remove(roleToManage)
-            interaction.reply(
-                `Role, ${roleToManage} has been ${
-                    action === 'give' ? 'given to' : 'taken from'
-                } ${user}`
+async function roleManage(
+    interaction: CommandInteraction,
+    action: 'give' | 'take',
+    role: Role,
+    user: GuildMember
+) {
+    const manageableRoles = await prisma.manageableRoles.findMany({
+        where: { roleId: role.id },
+        select: { managerRoleId: true }
+    })
+
+    const canManageRole = manageableRoles.some((manageableRole) => {
+        if (
+            !(
+                interaction.member &&
+                interaction.member instanceof GuildMember &&
+                interaction.member.roles instanceof GuildMemberRoleManager
             )
-        } catch (err) {
-            if (err instanceof DiscordAPIError)
-                return interaction.reply(
-                    err.code === 50001 ? `Sorry, I don't have permission to do that` : err.message
-                )
-        }
+        )
+            return false
+        return (
+            interaction.member.permissions.has('ManageRoles', true) ||
+            interaction.member.roles.cache.some((role) => role.id === manageableRole.managerRoleId)
+        )
+    })
+
+    if (!canManageRole) return interaction.reply(`Sorry, you don't have permession to do that`)
+
+    try {
+        action === 'give' ? await user.roles.add(role) : await user.roles.remove(role)
+        interaction.reply(`Role, ${role} ${action === 'give' ? 'given to' : 'taken from'} ${user}`)
+    } catch (err) {
+        if (err instanceof DiscordAPIError)
+            return interaction.reply(
+                err.code === 50001 ? `Sorry, I don't have permission to do that` : err.message
+            )
     }
 }
